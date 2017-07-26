41f45f2f62d327cb3561552578f9d332e92eb028
Abel Berhane <aberhane@datadimensions.com>
2017-07-26 12:33:52 -0400

    Mole Destroyer Game. A lot of fun and Simple to make.

---
+++ MoleDestroyer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MoleDestroyer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblHit = new System.Windows.Forms.Label();
+            this.lblMiss = new System.Windows.Forms.Label();
+            this.Mole = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Mole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHit
+            // 
+            this.lblHit.AutoSize = true;
+            this.lblHit.BackColor = System.Drawing.Color.Transparent;
+            this.lblHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHit.Location = new System.Drawing.Point(450, 18);
+            this.lblHit.Name = "lblHit";
+            this.lblHit.Size = new System.Drawing.Size(55, 25);
+            this.lblHit.TabIndex = 0;
+            this.lblHit.Text = "Hit: ";
+            // 
+            // lblMiss
+            // 
+            this.lblMiss.AutoSize = true;
+            this.lblMiss.BackColor = System.Drawing.Color.Transparent;
+            this.lblMiss.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiss.Location = new System.Drawing.Point(450, 43);
+            this.lblMiss.Name = "lblMiss";
+            this.lblMiss.Size = new System.Drawing.Size(68, 25);
+            this.lblMiss.TabIndex = 2;
+            this.lblMiss.Text = "Miss:";
+            // 
+            // Mole
+            // 
+            this.Mole.BackColor = System.Drawing.Color.Transparent;
+            this.Mole.Image = global::MoleDestroyer.Properties.Resources.alive;
+            this.Mole.Location = new System.Drawing.Point(81, 319);
+            this.Mole.Name = "Mole";
+            this.Mole.Size = new System.Drawing.Size(90, 100);
+            this.Mole.TabIndex = 3;
+            this.Mole.TabStop = false;
+            this.Mole.Click += new System.EventHandler(this.gotMole);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.moveMole);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MoleDestroyer.Properties.Resources.ground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(591, 547);
+            this.Controls.Add(this.Mole);
+            this.Controls.Add(this.lblMiss);
+            this.Controls.Add(this.lblHit);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mole Destroyer";
+            ((System.ComponentModel.ISupportInitialize)(this.Mole)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHit;
+        private System.Windows.Forms.Label lblMiss;
+        private System.Windows.Forms.PictureBox Mole;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MoleDestroyer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MoleDestroyer
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        Random rnd = new Random();
+        int locationNum = 0;
+        int score = 0;
+        int misses = 0;
+        bool isHit = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Logic for Mole being hit.
+        private void gotMole(object sender, EventArgs e)
+        {
+            //Increment score and show the dead image when Hit.
+            score++;
+            Mole.Image = Properties.Resources.dead;
+
+            isHit = true;
+            Mole.Enabled = false;
+        }
+
+        //
+        private void moveMole(object sender, EventArgs e)
+        {
+            //Score and Miss count is attached to the label
+            lblHit.Text = "Hit: " + score;
+            lblMiss.Text = "Miss: " + misses;
+
+            //If you miss, Add 1 to the Miss Counter
+            if (isHit == false)
+            {
+                misses++;
+            }
+
+            //If you score more than 15 points, Message showing you win pops up
+            if (score > 15)
+            {
+                timer1.Stop();
+                Mole.Enabled = false;
+                MessageBox.Show("You Win");
+            }
+            //If you miss more than 10 times, Message showing you lose pops up
+            else if (misses > 10)
+            {
+                timer1.Stop();
+                Mole.Enabled = false;
+                MessageBox.Show("You Lose");
+            }
+            
+            //The mole is moved regardless if they hit or miss
+            moveMole();
+        }
+
+        //Logic for moving the Mole. 
+        private void moveMole()
+        { 
+            // When you move a Mole, reset it to not hit and show the Alive Image.
+            isHit = false;
+            Mole.Enabled = true;
+            Mole.Image = Properties.Resources.alive;
+            Mole.BackColor = System.Drawing.Color.Transparent;
+
+            //Randomly selects one of the 6 locations below to move the Mole to.
+            locationNum = rnd.Next(1, 7);
+            switch (locationNum)
+            {
+                case 1:
+                    Mole.Left = 434;
+                    Mole.Top = 249;
+                    break;
+
+                case 2:
+                    Mole.Left = 257;
+                    Mole.Top = 211;
+                    break;
+
+                case 3:
+                    Mole.Left = 58;
+                    Mole.Top = 240;
+                    break;
+
+                case 4:
+                    Mole.Left = 85;
+                    Mole.Top = 318;
+                    break;
+
+                case 5:
+                    Mole.Left = 254;
+                    Mole.Top = 364;
+                    break;
+
+                case 6:
+                    Mole.Left = 416;
+                    Mole.Top = 323;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
